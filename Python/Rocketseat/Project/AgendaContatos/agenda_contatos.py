3ab3f1bd55f2ef128a28c6f9d93b87791b8f399b
MrJCRJ <mr.jociriju@gmail.com>
2024-01-16 14:34:20 -0300

    adicionando escolha 1 e criando as class contato e agenda

--- Python/Rocketseat/Project/AgendaContatos/agenda_contatos.py
+++ Python/Rocketseat/Project/AgendaContatos/agenda_contatos.py
@@ -1,3 +1,20 @@
+class Contato:
+    def __init__(self, nome, telefone, email, favorito=False):
+        self.nome = nome
+        self.telefone = telefone
+        self.email = email
+        self.favorito = favorito
+
+class Agenda:
+    def __init__(self):
+        self.contatos = []
+
+    def adicionar_contato(self, contato):
+        self.contatos.append(contato)
+
+
+
+agenda = Agenda()
 while True:
     print("\n1. Adicionar Contato")
     print("2. Visualizar Contatos")
@@ -12,3 +29,29 @@ while True:
     
     if escolha == "0":
         break
+
+    elif escolha == "1":
+        nome = input("Nome: ")
+        telefone = input("Telefone: ")
+        email = input("Email: ")
+        favorito = input("É favorito? (S/N): ").upper() == "S"
+        novo_contato = Contato(nome, telefone, email, favorito)
+        agenda.adicionar_contato(novo_contato)
+
+    elif escolha == "2":
+        pass
+
+    elif escolha == "3":
+        pass
+
+    elif escolha == "4":
+        pass
+
+    elif escolha == "5":
+        pass
+
+    elif escolha == "6":
+        pass
+
+    else:
+        print("Opção inválida. Tente novamente.")
