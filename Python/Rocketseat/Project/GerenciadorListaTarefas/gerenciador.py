66055ec6dc28d95caddf3c6cdd7014c9e867ed19
MrJCRJ <mr.jociriju@gmail.com>
2024-01-18 09:53:45 -0300

    implementação do ver tarefas

--- Python/Rocketseat/Project/GerenciadorListaTarefas/gerenciador.py
+++ Python/Rocketseat/Project/GerenciadorListaTarefas/gerenciador.py
@@ -4,6 +4,14 @@ def adicionar_tarefas(tarefas, nome_tarefa):
     print(f"Tarefa {nome_tarefa} foi adicionada com sucesso")
     return
 
+def ver_tarefas(tarefas):
+    print("\nLista de Tarefas:")
+    for indice, tarefa in enumerate(tarefas, start=1):
+        status =  "✓" if tarefa["completado"] else " "
+        nome_tarefa = tarefa["tarefa"]
+        print(f"{indice}. [{status}] {nome_tarefa}")
+    return
+
 def menu():
     print("\n Menu do Gerenciador de Lista de Tarefas: ")
     print("1. Adicionar Tarefa")
@@ -25,6 +33,15 @@ while True:
     elif escolha == "1":
         nome_tarefa = input("Digite o nome da tarefa deseja adicionar: ")
         adicionar_tarefas(tarefas, nome_tarefa)
+
+    elif escolha == "2":
+        ver_tarefas(tarefas)
+    elif escolha == "3":
+        pass
+    elif escolha == "4":
+        pass
+    elif escolha == "5":
+        pass
 
 
 print('Programa finalizado')
