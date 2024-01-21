f22d06511cc89a22b936c485be783905d04b7daa
MrJCRJ <mr.jociriju@gmail.com>
2024-01-21 18:59:13 -0300

    update

--- Python/Rocketseat/Project/Flask/app.py
+++ Python/Rocketseat/Project/Flask/app.py
@@ -33,5 +33,20 @@ def get_task(id):
         
     return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
 
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    
+    if task == None: 
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+
 if __name__ == "__main__":
     app.run(debug=True)
