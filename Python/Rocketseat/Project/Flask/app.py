6c31ad5f6d025461ada6076746fa7d4881971789
MrJCRJ <mr.jociriju@gmail.com>
2024-01-21 18:15:39 -0300

    read

--- Python/Rocketseat/Project/Flask/app.py
+++ Python/Rocketseat/Project/Flask/app.py
@@ -20,9 +20,18 @@ def get_tasks():
     task_list = [task.to_dict() for task in tasks]
     output = {
         "tasks": task_list,
-        "total_tasks":0
+        "total_tasks":len(task_list)
     }
     return jsonify(output)
+
+@app.route("/tasks/<int:id>", methods=["GET"])
+def get_task(id):
+
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+        
+    return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
