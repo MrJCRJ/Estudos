b2d5f24ea2d462d68520eeeca8a7774043994099
MrJCRJ <mr.jociriju@gmail.com>
2024-01-23 09:27:47 -0300

    Gerenciamento de Perfil

--- Python/Rocketseat/Project/Flask/sample-flask-auth/app.py
+++ Python/Rocketseat/Project/Flask/sample-flask-auth/app.py
@@ -53,7 +53,7 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        user = User(username=username, password=password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({'message': 'Usuario cadastrado com sucesso'})
@@ -76,6 +76,9 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role == "user":
+        return jsonify({"message": "Operação não permitida"}), 403
+
     if user and data.get("password"):
         user.password = data.get("password")
         db.session.commit()
@@ -88,6 +91,9 @@ def update_user(id_user):
 @login_required
 def delete_user(id_user):
     user = User.query.get(id_user)
+
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida"}), 403
 
     if id_user == current_user.id:
         return jsonify({'message': 'Deleção não permitida'}), 403
--- Python/Rocketseat/Project/Flask/sample-flask-auth/models/user.py
+++ Python/Rocketseat/Project/Flask/sample-flask-auth/models/user.py
@@ -5,3 +5,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=True)
     password = db.Column(db.String(80), nullable=False)
+    role = db.Column(db.String(80), nullable=False, default='user')
