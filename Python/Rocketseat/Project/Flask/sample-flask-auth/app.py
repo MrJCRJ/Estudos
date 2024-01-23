4e5049bdb7ba58b0adb6a3b0d1201a5350170631
MrJCRJ <mr.jociriju@gmail.com>
2024-01-23 02:59:51 -0300

    Autenticação com Flask - login

--- Python/Rocketseat/Project/Flask/sample-flask-auth/app.py
+++ Python/Rocketseat/Project/Flask/sample-flask-auth/app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from models.user import User
 from database import db
 from config import Config
+from flask_login import LoginManager
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login_manager = LoginManager()
+
 db.init_app(app)
+login_manager.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+
+    return jsonify({"message":"Credenciais inválidas"}), 400
 
 @app.route("/create_db", methods=["GET"])
 def create_db():
