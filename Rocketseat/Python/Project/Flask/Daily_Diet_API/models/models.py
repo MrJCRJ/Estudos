49de0a3301e91cddff7ea2514758e78a8595e7a0
MrJCRJ <mr.jociriju@gmail.com>
2024-01-30 16:52:21 -0300

    Mundando de pasta

---
+++ Rocketseat/Python/Project/Flask/Daily_Diet_API/models/models.py
@@ -0,0 +1,9 @@
+from main import db
+from datetime import datetime
+
+class Meal(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(50), nullable=False)
+  description = db.Column(db.String(200))
+  date_time = db.Column(db.DateTime, default=datetime.utcnow)
+  is_within_diet = db.Column(db.Boolean, default=True)
